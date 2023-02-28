560dadab31c676424d2889f3c76e1e390d5d2adb
vado dev <marvin.wekesa@gamail.com>
2023-02-28 20:13:00 +0300

    added functionality to save favourite keys for snapshot

--- snipsnap.py
+++ snipsnap.py
@@ -9,6 +9,7 @@ import subprocess
 # Create directory to store the screenshots. Directory name is based on the date and time
 directory = time.strftime("%Y-%m-%d_%H-%M-%S")
 os.makedirs("snapshots/" + directory)
+key_path = os.path.exists("keylog.txt")
 
 # Define function to capture a screenshot of the active window
 def capture_screenshot():
@@ -29,11 +30,30 @@ def capture_screenshot():
 
 # Define a function to handle key presses
 def on_press(key):
-    try:
+    try:       
         # Default key: f3
         # Replace f3 with any key you want to use (e.g. shift_r, alt_l, ctrl_r, esc, backspace, enter, down, up, esc, etc.)
-        if key == keyboard.Key.f3:
-            capture_screenshot()
+        if(key_path):
+            with open("key_compare", "w", encoding="utf-8") as f:
+                f.write('{0}'.format(key))
+            with open("keylog.txt", "r") as f:
+                logged_key = f.read()
+                with open("key_compare", "r") as f:
+                    compare_key = f.read()
+                    if(compare_key == logged_key):
+                        #print("All good")
+                        capture_screenshot()
+                    else:
+                        print("Please press ", logged_key)
+
+                #print(logged_key)
+                
+        else:
+            with open("keylog.txt", "w", encoding="utf-8") as f:
+                f.write('{0}'.format(key))
+        # if key == keyboard.Key.f3:
+        #     capture_screenshot()
+        #     #print(f'{key} pressed')
     except AttributeError:
         pass
 
